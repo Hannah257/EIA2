5d4efb1ef3f02a463a95b3556a8cd88972155363
Hannah257 <Hannah257@users.noreply.github.com>
2020-05-26 08:12:01 +0200

    commit

---
+++ L06_Server/L06_Test/Convert.js
@@ -0,0 +1,9 @@
+"use strict";
+var L06Convert;
+(function (L06Convert) {
+    let x = 23;
+    console.log(x);
+    let parsed = parseInt("A4", 18);
+    console.log(parsed);
+})(L06Convert || (L06Convert = {}));
+//# sourceMappingURL=Convert.js.map
---
+++ L06_Server/L06_Test/Convert.ts
@@ -0,0 +1,7 @@
+namespace L06Convert {
+    let x: number = 23; 
+    console.log(x); 
+
+    let parsed: number = parseInt("A4", 18); 
+    console.log(parsed); 
+}
