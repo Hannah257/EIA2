3be0ebbc458fc598fcf35c0b6d306c45aa9bc5c8
Hannah257 <Hannah257@users.noreply.github.com>
2020-06-09 10:16:37 +0200

    delete

--- L08_Canvas/L08_Code.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let canvas = document.querySelector("canvas");
-let crc2 = canvas.getContext("2d");
-crc2.fillStyle = "#FF0000";
-crc2.fillRect(100, 100, crc2.canvas.width, crc2.canvas.height);
-//# sourceMappingURL=L08_Code.js.map
