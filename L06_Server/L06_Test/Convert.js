444bb6e9a52e20a16f022a2864dc747059bdebeb
Hannah257 <56229593+Hannah257@users.noreply.github.com>
2021-03-16 12:36:21 +0100

    Delete L06_Server directory

--- L06_Server/L06_Test/Convert.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var L06Convert;
-(function (L06Convert) {
-    let x = 23;
-    console.log(x);
-    let parsed = parseInt("A4", 18);
-    console.log(parsed);
-})(L06Convert || (L06Convert = {}));
-//# sourceMappingURL=Convert.js.map
--- L06_Server/L06_Test/Convert.ts
+++
@@ -1,7 +0,0 @@
-namespace L06Convert {
-    let x: number = 23; 
-    console.log(x); 
-
-    let parsed: number = parseInt("A4", 18); 
-    console.log(parsed); 
-}
