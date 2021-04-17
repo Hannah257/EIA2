046d71e300cfd3a8dc59df5e370e863a3d8b716e
Hannah257 <Hannah257@users.noreply.github.com>
2021-04-17 19:19:01 +0200

    Änderungen

--- 02_Eventinspector/02_Eventinspector_Code.ts
+++ 02_Eventinspector/02_Eventinspector_Code.ts
@@ -11,7 +11,7 @@ namespace L02_Eventinspector {
         let div: NodeListOf<HTMLDivElement> = document.querySelectorAll("div");
         for (let i: number = 0; i < div.length; i++) {
             div[i].addEventListener("click", logInfo);
-            div[i].addEventListener("click", logInfo);
+            div[i].addEventListener("keyup", logInfo);
         }
 
         document.addEventListener("mousemove", setInfoBox);
@@ -22,27 +22,21 @@ namespace L02_Eventinspector {
     function setInfoBox(_event: MouseEvent): void {
         let span: HTMLSpanElement = <HTMLSpanElement>document.querySelector("span");
         let target: HTMLElement = <HTMLElement>_event.target;
-        let clientX: number = _event.clientX;
-        let clientY: number = _event.clientY;
+        let clientX: number = _event.clientX + 20;
+        let clientY: number = _event.clientY + 20;
+
         span.innerHTML = "";
         span.innerHTML = clientX + "x" + clientY + "y" + target + "target";
+
+        span.style.left = clientX + "px";
+        span.style.top = clientY + "px";
     }
 
-    function logInfo(): void {
-        console.log("Platzhalter");
-        
-        console.log(_event.type);
-        console.log(_event.target);
-        console.log(_event.currentTarget);
+    function logInfo(_event: Event): void {
+        console.log("Target: " + _event.target);
+        console.log("CurrentTarget: " + _event.currentTarget);
+        console.log("Type Event: " + _event.type);
         console.log(_event);
-
     }
-
-
-
-
-
-
-
 
 }
