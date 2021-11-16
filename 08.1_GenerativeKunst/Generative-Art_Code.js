d4b48c9ead4e261fcd6c61c4a1acf84b65527962
Hannah257 <Hannah257@users.noreply.github.com>
2021-11-16 18:54:52 +0100

    Erste Versuche L08.1

---
+++ 08.1_GenerativeKunst/Generative-Art_Code.js
@@ -0,0 +1,51 @@
+"use strict";
+var L08_GenerativeArt;
+(function (L08_GenerativeArt) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        let canvas = document.querySelector("canvas");
+        let crc2 = canvas.getContext("2d");
+        let farbpalette = ["lightblue", "cadetblue", "cornflowerblue", "darkgrey"];
+        for (let index = 0; index < 50; index++) {
+            let x = Math.floor(Math.random());
+            let y = Math.floor(Math.random());
+            let a = Math.floor(Math.random());
+            let b = Math.floor(Math.random());
+            let a1 = Math.floor(Math.random());
+            let b1 = Math.floor(Math.random());
+            let getColor = Math.floor(Math.random() * Math.floor(4));
+            crc2.beginPath();
+            crc2.arc(x, y, 150, 0, 2 * Math.PI, false);
+            crc2.fillStyle = farbpalette[getColor];
+            crc2.strokeStyle = farbpalette[getColor];
+            crc2.moveTo(75, 50);
+            crc2.lineTo(100, 75);
+            crc2.fill();
+            crc2.closePath();
+            crc2.beginPath();
+            crc2.strokeStyle = farbpalette[getColor];
+            crc2.moveTo(75, 25);
+            crc2.quadraticCurveTo(25, 25, 25, 62.5);
+            crc2.quadraticCurveTo(25, 100, 50, 100);
+            crc2.quadraticCurveTo(50, 120, 30, 125);
+            crc2.quadraticCurveTo(60, 120, 65, 100);
+            crc2.quadraticCurveTo(125, 100, 125, 62.5);
+            crc2.quadraticCurveTo(125, 25, 75, 25);
+            crc2.stroke();
+            crc2.beginPath();
+            crc2.strokeStyle = farbpalette[getColor];
+            crc2.setLineDash([20, 15]);
+            crc2.moveTo(x, y);
+            crc2.lineTo(a, b);
+            crc2.lineTo(a1, b1);
+            crc2.stroke();
+            crc2.beginPath();
+            crc2.arc(x, y, 150, 0, 2 * Math.PI, false);
+            crc2.fillStyle = farbpalette[getColor];
+            crc2.strokeStyle = farbpalette[getColor];
+            crc2.strokeRect(50, 50, 50, 50);
+            crc2.closePath();
+        }
+    }
+})(L08_GenerativeArt || (L08_GenerativeArt = {}));
+//# sourceMappingURL=Generative-Art_Code.js.map
---
+++ 08.1_GenerativeKunst/Generative-Art_Code.ts
@@ -0,0 +1,59 @@
+namespace L08_GenerativeArt {
+    window.addEventListener("load", handleLoad);
+    
+    function handleLoad(): void {
+        let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.querySelector("canvas");
+        let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D> canvas.getContext("2d");
+        
+    
+        let farbpalette: string[] = ["lightblue", "cadetblue", "cornflowerblue", "darkgrey"];
+    
+        for (let index: number = 0; index < 50; index++) {
+            let x: number = Math.floor(Math.random());
+            let y: number = Math.floor(Math.random());
+            let a: number = Math.floor(Math.random());
+            let b: number = Math.floor(Math.random());
+            let a1: number = Math.floor(Math.random());
+            let b1: number = Math.floor(Math.random());
+    
+            let getColor: number = Math.floor(Math.random() * Math.floor(4));
+
+            crc2.beginPath();
+            crc2.arc( x, y, 150, 0, 2 * Math.PI , false);
+            crc2.fillStyle = farbpalette[getColor];
+            crc2.strokeStyle = farbpalette[getColor];
+            crc2.moveTo(75, 50);
+            crc2.lineTo(100, 75);
+            crc2.fill();
+            crc2.closePath();
+            crc2.stroke();
+
+            crc2.beginPath();
+            crc2.strokeStyle = farbpalette[getColor];
+            crc2.moveTo(75, 25);
+            crc2.quadraticCurveTo(25, 25, 25, 62.5);
+            crc2.quadraticCurveTo(25, 100, 50, 100);
+            crc2.quadraticCurveTo(50, 120, 30, 125);
+            crc2.quadraticCurveTo(60, 120, 65, 100);
+            crc2.quadraticCurveTo(125, 100, 125, 62.5);
+            crc2.quadraticCurveTo(125, 25, 75, 25);
+            crc2.stroke();
+
+            crc2.beginPath();
+            crc2.strokeStyle = farbpalette[getColor];
+            crc2.setLineDash([20, 15]);
+            crc2.moveTo(x, y);
+            crc2.lineTo(a, b);
+            crc2.lineTo(a1, b1);
+            crc2.stroke();
+
+            crc2.beginPath();
+            crc2.arc( x, y, 150, 0, 2 * Math.PI , false);
+            crc2.fillStyle = farbpalette[getColor];
+            crc2.strokeStyle = farbpalette[getColor];
+            crc2.strokeRect(50, 50, 50, 50);
+            crc2.closePath();
+            crc2.stroke();
+        }
+    }
+}
